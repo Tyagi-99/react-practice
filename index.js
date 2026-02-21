6b45478358db1a469723fc0e7a59290a6e68f7ea
tyagi-99 <tyagihusain6398@gmail.com>
2026-02-21 15:51:32 +0530

    Added first React promgram using Parcel

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-console.log("Hello GitHub!");
+import React from "react";
+import ReactDOM from "react-dom/client"
+
+const heading = React.createElement("h1",
+    {id: "heading"},
+    "i am very happy today cause i write my first promgram "
+);
+ 
+const root = ReactDOM.createRoot(document.getElementById("root"))
+root.render(heading)
