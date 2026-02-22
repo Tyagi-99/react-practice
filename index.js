7a76777959d6549cdd6925ff3fb649083ccf31f6
hakker7037 <husain7037@gmail.com>
2026-02-22 14:40:59 +0530

    this is my first Jsx

--- index.js
+++ index.js
@@ -1,10 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client"
 
-const heading = React.createElement("h1",
-    {id: "heading"},
-    "i am very happy today cause i write my first promgram "
-);
- 
-const root = ReactDOM.createRoot(document.getElementById("root"))
-root.render(heading)
+
+const firstjxs = <h2>i am using Jsx now </h2>
+const root = ReactDOM.createRoot(document.getElementById("jsx"))
+root.render(firstjxs)
